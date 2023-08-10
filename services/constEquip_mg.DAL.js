d192fc1ce4e87bdebda2c0b01394f7e91064948a
Sixsinglebird <97618579+Sixsinglebird@users.noreply.github.com>
2023-08-10 11:24:39 -0230

    mongo has a bug but werkz

--- services/constEquip_mg.DAL.js
+++ services/constEquip_mg.DAL.js
@@ -1,5 +1,6 @@
 ////////////////////////////////////////////////
-import { client } from "./constEquip_mg.DB_ACCESS";
+const { client } = require("./constEquip_mg.DB_ACCESS");
+const { ObjectId } = require("mongodb");
 
 ////////////////////////////////////////////////
 //  connect to database
@@ -17,21 +18,84 @@ const dbName = "construction_equipment";
 const collectionName = "cost_details";
 
 // Define the function to get equipment data
-const getEquipment = (req, res) => {
+const getEquipment = async (req, res) => {
   const db = client.db(dbName);
   const collection = db.collection(collectionName);
 
-  // Implement the logic to retrieve equipment data
-  collection
+  // Fetch equipment data from the collection
+  await collection
     .find({})
     .toArray()
     .then((equipment) => {
-      res.json(equipment);
+      // Respond with JSON data
+      res.status(200).json({
+        success: true,
+        message: "Equipment data retrieved successfully",
+        data: equipment,
+      });
     })
     .catch((error) => {
       console.error("Error retrieving equipment data:", error);
-      res.status(500).json({ error: "Internal Server Error" });
+      res.status(500).json({
+        success: false,
+        error: "Internal Server Error",
+      });
     });
 };
 
-export default { getEquipment };
+// Define the function to delete equipment data
+const deleteEquipment = async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+
+  const equipmentId = req.params.id; // Assuming you're passing the equipment ID in the URL
+
+  try {
+    const deleteResult = await collection.deleteOne({
+      _id: new ObjectId(equipmentId),
+    });
+    if (deleteResult.deletedCount > 0) {
+      res.status(200).json({
+        success: true,
+        message: "Equipment data deleted successfully",
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        error: "Equipment not found",
+      });
+    }
+  } catch (error) {
+    console.error("Error deleting equipment data:", error);
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
+
+// Define the function to add equipment data
+const addEquipment = async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+
+  // Assuming you have the equipment data in the request body
+  const equipmentData = req.body;
+
+  try {
+    const insertResult = await collection.insertOne(equipmentData);
+    res.status(201).json({
+      success: true,
+      message: "Equipment data added successfully",
+      data: insertResult.ops[0], // Return the inserted equipment data
+    });
+  } catch (error) {
+    console.error("Error adding equipment data:", error);
+    res.status(500).json({
+      success: false,
+      error: "Internal Server Error",
+    });
+  }
+};
+
+module.exports = { getEquipment, addEquipment, deleteEquipment };
--- src/routes/equipmentRoutes.js
+++ src/routes/equipmentRoutes.js
@@ -1,17 +1,37 @@
 ////////////////////////////////////////////////
 const { Router } = require("express");
 const router = Router();
-const dal = require("../../services/constEquip_mg.DB_ACCESS");
+const dal = require("../../services/constEquip_mg.DAL");
 
 ////////////////////////////////////////////////
 router.get("/", async (req, res) => {
   try {
-    if (DEBUG) console.log(`post request - ${req.url}`);
+    if (DEBUG) console.log(`get request - ${req.url}`);
 
     await dal.getEquipment(req, res);
-    res.status(200).send();
   } catch (error) {
     console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    if (DEBUG) console.log(`Put request - ${req.url}`);
+
+    await dal.addEquipment(req, res);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    if (DEBUG) console.log(`delete request - ${req.url}`);
+    await dal.deleteEquipment(req, res);
+  } catch (error) {
+    console.error("Error deleting equipment:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
