73f1cc08a4c18b162ce080438ea6fce564370742
Sixsinglebird <97618579+Sixsinglebird@users.noreply.github.com>
2023-08-10 02:13:41 -0230

    updated dependencies and scripts
    needed nodemon for the sake of my sanity

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-const { MongoDBUser } = require('./user'); // For MongoDB
-const { Pool } = require('pg'); // For PostgreSQL
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+// const { MongoDBUser } = require("./user"); // For MongoDB
+const { Pool } = require("pg"); // For PostgreSQL
 
 passport.use(
   new LocalStrategy(async (username, password, done) => {
@@ -11,16 +11,21 @@ passport.use(
 
       if (db === req.mongoose) {
         const user = await MongoDBUser.findOne({ username: username });
-        if (!user) return done(null, false, { message: 'Incorrect username.' });
+        if (!user) return done(null, false, { message: "Incorrect username." });
         const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) return done(null, false, { message: 'Incorrect password.' });
+        if (!passwordMatch)
+          return done(null, false, { message: "Incorrect password." });
         return done(null, user);
       } else if (db === req.pool) {
-        const result = await db.query('SELECT * FROM users WHERE username = $1', [username]);
+        const result = await db.query(
+          "SELECT * FROM users WHERE username = $1",
+          [username]
+        );
         const user = result.rows[0];
-        if (!user) return done(null, false, { message: 'Incorrect username.' });
+        if (!user) return done(null, false, { message: "Incorrect username." });
         const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) return done(null, false, { message: 'Incorrect password.' });
+        if (!passwordMatch)
+          return done(null, false, { message: "Incorrect password." });
         return done(null, user);
       }
     } catch (error) {
@@ -41,7 +46,7 @@ passport.deserializeUser(async (id, done) => {
       const user = await MongoDBUser.findById(id);
       done(null, user);
     } else if (db === req.pool) {
-      const result = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+      const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
       const user = result.rows[0];
       done(null, user);
     }
